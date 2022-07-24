17184b0744765ce2d9de22328988d2a03e46ced8
vsoch <vsoch@users.noreply.github.com>
2022-07-24 00:03:14 -0600

    create temporary files and cleanup for better singularity usage
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 old=$1
-new=$2
+new=$2u
 
 if [ -z "${old+xxx}" ]; then echo "Missing first argument, old library"; exit 1; fi
 if [ -z "${new+xxx}" ]; then echo "Missing second argument, new library"; exit 1; fi
 
-abi-dumper $old -o ABI-1.dump -lver 1
-abi-dumper $new -o ABI-2.dump -lver 2
-abi-compliance-checker -l NAME -old ABI-1.dump -new ABI-2.dump
+dump_old=$(mktemp /tmp/ABI-1-XXXXX.dump)
+dump_new=$(mktemp /tmp/ABI-2-XXXXX.dump)
+
+abi-dumper $old -o $dump_old -lver 1
+abi-dumper $new -o $dump_new -lver 2
+abi-compliance-checker -l NAME -old $dump_old -new $dump_new
+rm $dump_old
+rm $dump_new
