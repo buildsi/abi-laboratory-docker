65c00aa9147e30ae8f53c7446dc1c4fbe3ae518e
vsoch <vsoch@users.noreply.github.com>
2022-07-25 12:04:08 -0600

    add options links for future people to look up
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,11 @@ dump_old=$(mktemp /tmp/ABI-1-XXXXX.dump)
 dump_new=$(mktemp /tmp/ABI-2-XXXXX.dump)
 report_path=$(mktemp /tmp/report-XXXXX.html)
 
+# Options: https://github.com/lvc/abi-dumper/blob/master/abi-dumper.pl#L112
 abi-dumper $old -o $dump_old -lver 1
 abi-dumper $new -o $dump_new -lver 2
+
+# Options: https://github.com/lvc/abi-compliance-checker/blob/master/abi-compliance-checker.pl#L119
 abi-compliance-checker -l $name -old $dump_old -new $dump_new -report-path $report_path
 rm $dump_old
 rm $dump_new
